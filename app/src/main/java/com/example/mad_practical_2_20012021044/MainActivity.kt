040d1aba3a1611580137aafc51381c3561c34ba3
vaibhav3289 <110806561+vaibhav3289@users.noreply.github.com>
2022-09-13 21:54:58 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/mad_practical_2_20012021044/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_2_20012021044/MainActivity.kt
@@ -7,55 +7,66 @@ import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.snackbar.Snackbar
 
+
 class MainActivity : AppCompatActivity() {
-    private var TAG="mainactivity"
+    private val TAG ="MainActivity"
     private lateinit var mycl:ConstraintLayout
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mycl=findViewById(R.id.mycl)
-        Log.i(TAG, "onCreate:onCreate method is called ")
-        Toast.makeText(this,"onCreate:onCreate method is called ", Toast.LENGTH_LONG).show()
-        Snackbar.make(mycl,"onCreate method is called",Snackbar.LENGTH_LONG).show()
-    }
+        Log.i(TAG, "onCreate function call")
+        Toast.makeText(this, "onCreate function call",
+            Toast.LENGTH_SHORT).show()
 
+        Snackbar.make(mycl,"onCreate function " +
+                "call",Snackbar.LENGTH_LONG).show()
+    }
     override fun onStart() {
         super.onStart()
+        Log.i(TAG, "onStart function call")
+        Toast.makeText(this, "onStart function call",
+            Toast.LENGTH_SHORT).show()
 
-        Log.i(TAG, "onCreate:onCreate method is called ")
-        Toast.makeText(this,"onCreate:onCreate method is called ", Toast.LENGTH_LONG).show()
-        Snackbar.make(mycl,"onCreate method is called",Snackbar.LENGTH_LONG).show()
-    }
-
-    override fun onResume() {
-        super.onResume()
-
-        Log.i(TAG, "onCreate:onCreate method is called ")
-        Toast.makeText(this,"onCreate:onCreate method is called ", Toast.LENGTH_LONG).show()
-        Snackbar.make(mycl,"onCreate method is called",Snackbar.LENGTH_LONG).show()
-    }
-
-    override fun onPause() {
-        super.onPause()
-
-        Log.i(TAG, "onCreate:onCreate method is called ")
-        Toast.makeText(this,"onCreate:onCreate method is called ", Toast.LENGTH_LONG).show()
-        Snackbar.make(mycl,"onCreate method is called",Snackbar.LENGTH_LONG).show()
+        Snackbar.make(mycl,"onStart function call",Snackbar.LENGTH_LONG).show()
     }
 
     override fun onStop() {
         super.onStop()
 
-        Log.i(TAG, "onCreate:onCreate method is called ")
-        Toast.makeText(this,"onCreate:onCreate method is called ", Toast.LENGTH_LONG).show()
-        Snackbar.make(mycl,"onCreate method is called",Snackbar.LENGTH_LONG).show()
+        Log.i(TAG, "onStop function call")
+        Toast.makeText(this, "onStop function call",
+            Toast.LENGTH_SHORT).show()
+        Snackbar.make(mycl,"onStop function call",Snackbar.LENGTH_LONG).show()
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "onPause function call")
+        Toast.makeText(this, "onPause function call",
+            Toast.LENGTH_SHORT).show()
+        Snackbar.make(mycl,"onPause function call",Snackbar.LENGTH_LONG).show()
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG, "onRestart function call")
+        Toast.makeText(this, "onRestart function call",
+            Toast.LENGTH_SHORT).show()
+        Snackbar.make(mycl,"onRestart function call",Snackbar.LENGTH_LONG).show()
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume function call")
+        Toast.makeText(this, "onResume function call",
+            Toast.LENGTH_SHORT).show()
+        Snackbar.make(mycl,"onResume function call",Snackbar.LENGTH_LONG).show()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-
-        Log.i(TAG, "onCreate:onCreate method is called ")
-        Toast.makeText(this,"onCreate:onCreate method is called ", Toast.LENGTH_LONG).show()
-        Snackbar.make(mycl,"onCreate method is called",Snackbar.LENGTH_LONG).show()
+        Log.i(TAG, "onDestroy function call")
+        Toast.makeText(this, "onDestroy function call",
+            Toast.LENGTH_SHORT).show()
+        Snackbar.make(mycl,"onDestroy function call",Snackbar.LENGTH_LONG).show()
     }
 }
